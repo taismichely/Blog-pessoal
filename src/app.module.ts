b6a025e4ea2f3c67697f8138cd56fa691d8ad21b
taismichely <michely2207@gmail.com>
2025-06-24 08:41:16 -0300

    Alteracao de arquivo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Postagem } from './postagem/entities/postagem.entity';
+import { PostagemModule } from './postagem/postagem.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_blogpessoal',
+      entities: [Postagem],
+      synchronize: true,
+    }),
+    PostagemModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
