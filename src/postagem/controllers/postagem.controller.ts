5cf29c9895901cc758d949057621e7d69129d7c3
taismichely <michely2207@gmail.com>
2025-06-24 20:57:39 -0300

    Adicionando metodos de cadastrar, atualizar e deletar

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,8 +1,19 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { Postagem } from '../entities/postagem.entity';
 import { PostagemService } from './../services/postagem.service';
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 
-@Controller('/postagem')
+@Controller('/postagens')
 export class PostagemController {
   constructor(private readonly PostagemService: PostagemService) {}
 
@@ -10,5 +21,37 @@ export class PostagemController {
   @HttpCode(HttpStatus.OK)
   findAll(): Promise<Postagem[]> {
     return this.PostagemService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  //Vai buscar o parametro com nome id, transformar ele em numero
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+    return this.PostagemService.findById(id);
+  }
+
+  @Get('/titulo/:titulo')
+  @HttpCode(HttpStatus.OK)
+  findByAllTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+    return this.PostagemService.findByAllTitulo(titulo);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  //Vai criar um json @Body, mas que deve seguir o padrao da classe postagem: Postagem
+  create(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.PostagemService.create(postagem);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.PostagemService.update(postagem);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.PostagemService.delete(id);
   }
 }
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, ILike, Repository } from 'typeorm';
 import { Postagem } from '../entities/postagem.entity';
-import { Repository } from 'typeorm';
 
 @Injectable()
 export class PostagemService {
@@ -11,7 +11,50 @@ export class PostagemService {
     private postagemRepository: Repository<Postagem>,
   ) {}
   //Adicionar método como promessa com async para classe 'postagem'
+  //Método listar tudo
   async findAll(): Promise<Postagem[]> {
     return await this.postagemRepository.find();
+  }
+
+  //Método listar por id
+  async findById(id: number): Promise<Postagem> {
+    const postagem = await this.postagemRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!postagem) {
+      throw new HttpException(
+        'Não existe esta postagem!',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return postagem;
+  }
+
+  async findByAllTitulo(titulo: string): Promise<Postagem[]> {
+    return await this.postagemRepository.find({
+      where: {
+        titulo: ILike(`%${titulo}%`),
+      },
+    });
+  }
+
+  //Método cadastrar
+  async create(postagem: Postagem): Promise<Postagem> {
+    return await this.postagemRepository.save(postagem);
+  }
+
+  //Método atualizar
+  async update(postagem: Postagem): Promise<Postagem> {
+    await this.findById(postagem.id);
+    return await this.postagemRepository.save(postagem);
+  }
+
+  //Método deletar
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findById(id);
+    return await this.postagemRepository.delete(id);
   }
 }
